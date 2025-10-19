1921676baf4511e95c589fc5d8c445eb75d3d1af
Parth <20parthgupta03@gmail.com>
2025-10-20 04:29:26 +0530

    Added the docker file and redis url

--- src/config/redisClient.js
+++ src/config/redisClient.js
@@ -1,5 +1,7 @@
 const { createClient } = require('redis');
-const client = createClient();
+const client = createClient({
+    url: process.env.REDIS_URL
+});
 
 client.on('error', (err) => console.log('Redis Client Error', err));
 client.connect();
