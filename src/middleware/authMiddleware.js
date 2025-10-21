d229e0f55224fad61f903c66bab9e2de3ead3012
Parth <20parthgupta03@gmail.com>
2025-10-22 01:56:26 +0530

    fix: removed auth key check from authMiddleware

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const authenticationKey = (req, res, next) => {
     const apiKey = req.headers['x-api-key'];
-    if (!apiKey || apiKey !== process.env.AUTH_KEY) {
+    if (apiKey !== process.env.AUTH_KEY) {
         return res.status(401).json({
             error: "Unauthorized access. Invalid API key."
         })
